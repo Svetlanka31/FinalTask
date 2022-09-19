50c49c91ae1ac7bc2c1bff6f7f8a77ec0f1ba4bf
Svetlana <kozhevnikova-svetlana85@bk.ru>
2022-09-19 19:26:49 +0300

    Added task

---
+++ task/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам.
+//  Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[]FirstArray = new string[size];
+string[]SecondArray = new string[FirstArray.Length];
+void FillArray(string[] array, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+       Console.WriteLine($"Введите {i+1} элемент массива: ");
+    array[i] = Convert.ToString(Console.ReadLine());
+     
+}
+Console.WriteLine('[' + string.Join(", ",array) +']');
+Console.WriteLine();
+    }
+    FillArray(FirstArray, size);
+void GetSecondArray(string[] FirstArr, string[] SecondArr)
+{
+    int count = 0;
+
+    for (int i = 0; i < FirstArr.Length; i++)
+    {
+    if(FirstArr[i].Length <= 3)
+        {
+        SecondArr[count] = FirstArr[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+   
+}
+}
+GetSecondArray(FirstArray, SecondArray);
+PrintArray(SecondArray);
